748302762636d31feb25bf38ae9720306242c706
Arup Mandal <40239193+arupmandal@users.noreply.github.com>
2022-03-15 00:29:24 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,36 @@
+
+
+let allMusic = [
+  {
+    name: "Kuch Baatein",
+    artist: "Jubin Nautiyal, Payal Dev",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Barsaat Ki Dhun",
+    artist: "Jubin Nautiyal",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Tumse Pyaar Karke",
+    artist: "Jubin Nautiyal, Payal Dev",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Rim Jhim",
+    artist: "Ami Mishra, Jubin Nautiyal",
+    img: "3",
+    src: "3"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
